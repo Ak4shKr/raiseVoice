5e65513199538f760371e9ce1d3fd46e04769266
Ak4shKr <akashkmr7667@gmail.com>
2024-08-17 02:30:17 +0530

    implemente d socket io for discussioin section

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import express from "express";
-const app = express();
+import http from "http";
+import { Server } from "socket.io";
 import routes from "./routes.js";
 import mongoose from "mongoose";
-
 import cloudinary from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
+app.use(express.static("public"));
 
 mongoose
   .connect("mongodb://localhost:27017/feedbackSystem")
@@ -15,9 +23,9 @@ mongoose
   .catch((error) => {
     console.log("Error: ", error);
   });
+
 app.use("/api", routes);
 
-// Return "https" URLs by setting secure: true
 cloudinary.config({
   secure: true,
 });
@@ -28,6 +36,19 @@ cloudinary.v2.config({
   api_secret: "5-qhz2zY8d405k0-SVQiaygUkMQ",
 });
 
-app.listen(3000, () => {
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("chat message", (data) => {
+    console.log("Message received:", data);
+    io.emit("chat message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
